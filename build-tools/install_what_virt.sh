30307c90cdfd3cc99ff453daaeb296ac4d0ba1d7
Jeff Bean <jeff.bean@hds.com>
2015-01-30 10:27:45 -0800

    More scripts to help recreate the disk adding blank script to install gather info script

--- build-tools/install_what_virt.sh
+++ build-tools/install_what_virt.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-mkdir -p ../stage/
-cd ../stage/
+
+cd /tmp/
+
 wget http://people.redhat.com/~rjones/virt-what/files/virt-what-1.14.tar.gz
 tar xvf virt-what-1.14.tar.gz
+rm -f virt-what-1.14.tar.gz
 cd virt-what-1.14
 ./configure
 make
